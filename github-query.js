fbb49d8c94d54b5594ef56ae8ad6f3f2cecaf361
Sidafa Conde <sconde@umassd.edu>
2015-08-28 23:01:39 -0600

    now can have a list of repositories to show, instead of all

--- github-query.js
+++ github-query.js
@@ -8,15 +8,21 @@ jQuery.fn.loadRepositories = function(username) {
     var target = this;
     $.githubUser(username, function(data) {
         var repos = data.data; // JSON Parsing
-        sortByName(repos);    
+        sortByName(repos); 
+
+        // list of repositories to show on the site
+        var listOfRepos = ["nodepy", "nonlinear-waves-course", "SSP_Tools", "numerical-mooc", "HeterogenousComputing"];
         
         var list = $('<dl/>');
         target.empty().append(list);
         $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.com')) {
+            //if (this.name == ('nodepy') || this.name == ("nonlinear-waves-course") || this.name == ("SSP_Tools")) {
+            if (~listOfRepos.indexOf(this.name)) {
                 list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + this.description +'</dd>');
-                //list.append('<dd>' + this.forks + '</dd>' );
+            list.append('<dd>' + this.forks_count + '</dd>');
+            list.append('<dd>' + this.description + '</dd>');
+            list.append('<dd>' + this.created_at + '</dd>');
+            list.append('<dd>' + this.watchers + '</dd>');
             }
         });
     });
